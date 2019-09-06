0ad6f20833a14c16acb3f22f9c4028a1d22e5c75
hegr54 <rigoberto.15212157@gmail.com>
2019-09-05 23:13:50 -0700

    Suseciones fibonacci Version iterativa vector y Version Divide y Vencers

--- Unidad 1/Practicas/Practica_3.scala
+++ Unidad 1/Practicas/Practica_3.scala
@@ -45,9 +45,54 @@ def fib3(num3:Int):Int = {
 def fib4(num4:Int):Int={
     var a=0
     var b=1
-    for( k <- Range(0,num3)){
+    for( k <- Range(0,num4)){
          b = b + a
          a = b - a
     }
     return a
 }
+
+////Quinta Sucesion fibonacci
+//
+def fib5(num5: Int): Int = {
+    val f: Array[Int] = Array.ofDim[Int](num5 + 2)
+    f(0) = 0
+    f(1) = 1
+    for (i <- Range (2, num5 + 1)) {
+      
+      f(i) = f(i - 1) + f(i - 2) //{ i += 1; i - 1 }
+    }
+    return f(num5)
+  }
+
+  ////Sexto sucecion Fibonacci
+
+  def fib6 (n : Int) : Double =
+{
+    if (n <= 0)
+    {
+        return 0
+    }
+    var i = n-1
+    var auxOne = 0.0
+    var auxTwo = 1.0
+    var ab = Array(auxTwo,auxOne)
+    var cd = Array(auxOne,auxTwo)
+    while (i>0)
+    {
+        if (i % 2 != 0)
+        {
+            auxOne = cd(1) * ab(1) + cd(0) * ab(0)
+            auxTwo = cd(1) * (ab(1)+ab(0)) + cd(0)*ab(1)
+            ab(0) = auxOne
+            ab(1) = auxTwo 
+        } 
+        auxOne = (math.pow(cd(0),2)) + (math.pow(cd(1),2))
+        auxTwo = cd(1)* (2*cd(0) + cd(1))
+        cd(0) = auxOne
+        cd(1) = auxTwo
+        i = i/2
+    }
+    return (ab(0) + ab(1))
+}
+println(fib1(10))
