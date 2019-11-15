22d405c57db1ff5e7d2223a838c2bf6d740f40f8
rigoberto <rigoberto.hernandez17@tectijuana.edu.mx>
2019-11-14 17:45:41 -0800

    EVALUACION 3 DOCUMENTADA

--- Unidad_2/Evaluacion/Examen_3.scala
+++ Unidad_2/Evaluacion/Examen_3.scala
@@ -1,5 +1,5 @@
-//Importacion del algoritmo Multilayer Perceotion Classifier y importamos las librerias importantes para generar 
-//1.- hacer la limpieza de datos necesaria para poder ser procesadocon el siguiente algoritmo
+//Importación del algoritmo Multilayer Perception Classifier y importamos las librerías importantes para generar 
+//1.- hacer la limpieza de datos necesaria para poder ser procesado con el siguiente algoritmo
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.sql.SparkSession
@@ -10,57 +10,56 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.types._
 import org.apache.log4j._
 Logger.getLogger("org").setLevel(Level.ERROR)
-//Creacion de la secion de spark.
+//Creación de la sección de spark.
 val Spark= SparkSession.builder().getOrCreate()
-//caraga y lectura de nuestro data set en un formato csv.
+//carga y lectura de nuestro dataset en un formato csv.
 val dataset = spark.read.option("header","true").option("inferSchema", "true")csv("Iris.csv")
-//visualizacion de nuestro data set.
+//visualización de nuestro data set.
 dataset.show()
-//Visualizacion del esque o como se encuentran nuestros datos.
+//Visualización del esquema o como se encuentran nuestros datos.
 dataset.printSchema()
-// se crea una variabje que contenbra el objeto structtype el cual puede tener uno o mas structfields se puede extraer por nombres, en caso de extraer varios
+// se crea una variable que contendrá el objeto struct type el cual puede tener uno o más structfields se puede extraer por nombres, en caso de extraer varios
 // structfield este se volvera un objeto structtype. Si uno de estos nombres que nosotros proporcionamos no contiene un campo
-//por coincidente se ignorara. para este caso de ectraer un solo Structfield se devolvera un valor nulo.
+//por coincidente se ignorará. para este caso de extraer un solo Structfield se devolverá un valor nulo.
 
 val datasetSchema =  /// estructura de un StructType
 StructType(
-StructField("Cinco/uno", DoubleType, true) :: /// el nombre del campo, tipo de dato en el que corresponde e incar si los valores de este capo seran valores nulos.
-StructField("Tres/tres", DoubleType, true) ::///los metadatos de este campo deben de conservase durante la trasformacion si el contenido de la columna no se modifican.
+StructField("Cinco/uno", DoubleType, true) :: /// el nombre del campo, tipo de dato en el que corresponda e indicar si los valores de este capo serán valores nulos.
+StructField("Tres/tres", DoubleType, true) ::///los metadatos de este campo deben de conservarse durante la transformación si el contenido de la columna no se modifican.
 StructField("Uno/cuatro", DoubleType, true) ::
 StructField("Cero/dos",DoubleType, true) ::
 StructField("Iris-setosa", StringType, true) :: Nil)
-/// carga y amalisis de Nuestro data set ya completamente listo para trabajar sobre el.
+/// carga y análisis de Nuestro dataset ya completamente listo para trabajar sobre el.
 val dataset2 = spark.read.option("header", "false").schema(datasetSchema)csv("Iris.csv")
 dataset2.columns
-//lectura de nuestro data set ya limpio.
-// creacion de nuestra etique con la que se trabajara en este algoritno.
+//lectura de nuestro dataset ya limpio.
+// creación de nuestra etiqueta con la que se trabajara en este algoritmo.
  val labelIndexer = new StringIndexer().setInputCol("Iris-setosa").setOutputCol("label").fit(dataset2)
- //caracteristicas con las que cuenta nuestra data set en cual sera un nuevo vectorAssember el cual contenbra nuestra entrada de datos con un arreglo el cual contendra nuestras caracteristicas y tendremos una salida de "factures" caracteristicas.
+ //características con las que cuenta nuestra dataset en cual será un nuevo vector Assembler el cual contendrá nuestra entrada de datos con un arreglo el cual contendrá nuestras características y tendremos una salida de "factures" características.
  val featureIndexer = new VectorAssembler().setInputCols(Array("Cinco/uno", "Tres/tres", "Uno/cuatro", "Cero/dos")).setOutputCol("features")
 
-//funcion de entrenamiento el cual sera 60% de entrenamiento y 40% de prueba con una profundidad o semilla de 1234L.
+//función de entrenamiento el cual será 60% de entrenamiento y 40% de prueba con una profundidad o semilla de 1234L.
 val splits = dataset2.randomSplit(Array(0.6, 0.4), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
 
-// Especificacion de nuestra capa de red neuronal de la siguiente manera:
-//la capa de entrada sera de tama;o 4 caracteristicas, dos capas oculta de tama;o 5 y 4, asi tendremos una salida de tama;o de 3 clases.
+// Especificación de nuestra capa de red neuronal de la siguiente manera:
+//la capa de entrada será de tamaño 4 características, dos capas oculta de tama;o 5 y 4, así tendremos una salida de tama;o de 3 clases.
 val layers = Array[Int](4, 5, 4, 3)
-//creamos el entrenador multilayerPercptronClassifier y establecimiento de sus parametros.
+//creamos el entrenador multilayerPercptronClassifier y establecimiento de sus parámetros.
 val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("label").setFeaturesCol("features").setPredictionCol("prediction").setBlockSize(128).setSeed(1234L).setMaxIter(100)
-//e.- explique la funcion de error que utilizo para el resultado final
+//e.- explique la función de error que utilizó para el resultado final
 val pipeline = new Pipeline().setStages(Array(labelIndexer,featureIndexer,trainer))
-//entrenamos el modelo de clasificacion de perceptron multicapa utilizando el estimador anterior.
+//entrenamos el modelo de clasificación de perceptrón multicapa utilizando el estimador anterior.
 val model = pipeline.fit(train)
-//calculamos la predicion en el conjunto de prueba.
+//calculamos la predicción en el conjunto de prueba.
 val result = model.transform(test)
-//impresion de resultados del calculo del conjunto de prueba.
+//impresión de resultados del cálculo del conjunto de prueba.
 result.show()
-//Evaluacion del modelo para el rendimiento de predicion.
+//Evaluación del modelo para el rendimiento de predicción.
 val predictionAndLabels = result.select("prediction", "label")
 predictionAndLabels.show()
-// Evaluador de nuestra predicion.
+// Evaluador de nuestra predicción.
 val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
-//Salida de la prueba de predicion.
+//Salida de la prueba de predicción.
 println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
-
